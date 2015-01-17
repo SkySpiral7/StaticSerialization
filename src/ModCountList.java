7b3f1444dbc6968458e7f9ad916c200954f243fa
Ryan Worcester <rworcest@g.emporia.edu>
2015-01-17 13:56:26 -0600

    Pasted some RandomAccess Iterators

---
+++ src/ModCountList.java
@@ -0,0 +1,13 @@
+package src;
+
+import java.util.AbstractList;
+import java.util.List;
+
+/**
+ * This interface was created so that any class may look at the modCount which is defined in AbstractList.
+ * @param <E> the data type of the list
+ * @see AbstractList
+ */
+public interface ModCountList<E> extends List<E> {
+	public int getModCount();
+}
