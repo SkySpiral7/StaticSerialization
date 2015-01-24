a272a64ac2945685c1c6d039d24247c0b1e55c23
Ryan Worcester <rworcest@g.emporia.edu>
2015-01-23 21:12:13 -0600

    InfInt: add(long). stubs. changed node<long> to int

--- src/InfiniteInteger.java
+++ src/InfiniteInteger.java
@@ -9,25 +9,40 @@ import src.defaultImplementations.DequeNode;
 import src.defaultImplementations.DequeNodeIterator;
 
 //aka InfinInt
-public class InfiniteInteger extends Number {
+//maxes: int[] 2^(32 * (2^31-1))-1 long[] 2^(64 * (2^31-1))-1
+//string (base 10): 10^(2^31-1) which is much smaller; big int: ?
+public class InfiniteInteger extends Number implements Copyable<InfiniteInteger> {
 	private static final long serialVersionUID = 1L;
+
 	public static final InfiniteInteger ZERO = new InfiniteInteger(0);
 	public static final InfiniteInteger NOT_A_NUMBER = new InfiniteInteger(false);
 	public static final InfiniteInteger POSITIVE_INFINITITY = new InfiniteInteger(true);
 	public static final InfiniteInteger NEGATIVE_INFINITITY = new InfiniteInteger(false);
+
+	//protected static final BigInteger bigIntegerMaxUnsignedLong = BigInteger.valueOf(Long.MAX_VALUE).shiftLeft(1).add(BigInteger.ONE);
 	protected static final BigInteger bigIntegerMaxLong = BigInteger.valueOf(Long.MAX_VALUE);
-	private static InfiniteInteger maxBigInteger;
+	//private static InfiniteInteger maxBigInteger;
+	/**This is also max unsigned integer*/
+	public static final int LOW_MASK_64 = 0xFFFF_FFFF;
+	//public static final long HIGH_MASK_64 = 0xFFFF_FFFF_0000_0000L;
 
 	/**
 	 * Little endian: the first node is the least significant.
 	 */
-	protected DequeNode<Long> magnitudeHead;
+	protected DequeNode<Integer> magnitudeHead;
 	protected boolean isNegative;
 
-	private InfiniteInteger(boolean isNegative){magnitudeHead = null; this.isNegative = isNegative;}
+	/**
+	 * This constructor is used to make special constants.
+	 * My making the head null it does something the other constructor can't.
+	 * @param isNegative used for positive and negative infinity. Meaningless to not a number.
+	 */
+	protected InfiniteInteger(boolean isNegative){magnitudeHead = null; this.isNegative = isNegative;}
 	protected InfiniteInteger(long value) {
 		isNegative = (value < 0);
-		magnitudeHead = DequeNode.Factory.createStandAloneNode(Math.abs(value));
+		value = Math.abs(value);
+		magnitudeHead = DequeNode.Factory.createStandAloneNode((int) (value & LOW_MASK_64));
+		if(value > LOW_MASK_64) DequeNode.Factory.createNodeAfter(magnitudeHead, ((int) (value >>> 32)));
 	}
 
 	public static InfiniteInteger valueOf(long value) {
@@ -64,31 +79,91 @@ public class InfiniteInteger extends Number {
 		return null;
 	}
 
-	public ListIterator<Long> magnitudeIterator() {
-		return new ReadOnlyListIterator<Long>(new DequeNodeIterator.IndexAgnosticValueIterator<Long>(magnitudeHead));
+	public ListIterator<Integer> magnitudeIterator() {
+		return new ReadOnlyListIterator<Integer>(new DequeNodeIterator.IndexAgnosticValueIterator<Integer>(magnitudeHead));
 	}
 
 	public InfiniteInteger add(long value) {
 		//TODO: currently assumes all positive
-		if(this == POSITIVE_INFINITITY || this == NOT_A_NUMBER || value == 0) return this;
-		long valueRemaining = value;
+		if(!this.isFinite() || value == 0) return this;
+		long sum, valueRemaining = value;
 		InfiniteInteger returnValue = new InfiniteInteger(0);  //can't use ZERO because this will be modified
-		DequeNode<Long> returnCursor = returnValue.magnitudeHead;
-		DequeNode<Long> thisCursor = this.magnitudeHead;
+		DequeNode<Integer> returnCursor = returnValue.magnitudeHead;
+		DequeNode<Integer> thisCursor = this.magnitudeHead;
+		int lowValue, highValue;
 		while (thisCursor != null)
 		{
-			long spaceAvailable = (Long.MAX_VALUE - thisCursor.getData());
-			if(spaceAvailable >= valueRemaining){returnCursor.setData(thisCursor.getData()+valueRemaining); valueRemaining = 0;}
-			returnCursor = DequeNode.Factory.createNodeAfter(returnCursor, 0L);
+			/*
+			int lowValue = (int) (valueRemaining & LOW_MASK_64);
+			int lowData = (int) (thisCursor.getData() & LOW_MASK_64);
+			long lowResult = lowData+lowValue;
+			int lowlowResult = (int) (lowResult & LOW_MASK_64);
+			int highlowResult = (int) (lowResult >>> 32);
+
+			int highValue = (int) (valueRemaining >>> 32);
+			int highData = (int) (thisCursor.getData() >>> 32);
+			long highResult = highData+highValue+highlowResult;
+			int lowhighResult = (int) (highResult & LOW_MASK_64);
+			valueRemaining = (int) (highResult >>> 32);
+
+			long result = (lowhighResult << 32) + lowlowResult;
+			*/
+
+			/*long spaceAvailable = (Integer.toUnsignedLong(Integer.MAX_UNSIGNED_VALUE) - Integer.toUnsignedLong(thisCursor.getData()));
+			if(spaceAvailable >= valueRemaining){
+				returnCursor.setData((int) (Integer.toUnsignedLong(thisCursor.getData())+Integer.toUnsignedLong((int) valueRemaining)));
+				valueRemaining = 0;}
+			returnCursor = DequeNode.Factory.createNodeAfter(returnCursor, 0);
+			thisCursor = thisCursor.getNext();*/
+
+			//turns out (true for signed and unsigned) max long > max int * max int. (2^64-1) > ((2^32-1)*(2^32-1))
+			if (valueRemaining != 0)
+			{
+				lowValue = ((int) (valueRemaining & LOW_MASK_64));
+				highValue = (int) (valueRemaining >>> 32);
+				sum = Integer.toUnsignedLong(thisCursor.getData()) + lowValue;
+
+				//TODO: test: is &low the same as (int)?
+				//TODO: confirm: is >>> 32 the number I want?
+				returnCursor.setData((int) (sum & LOW_MASK_64));
+				sum >>>=32;
+
+				valueRemaining = sum + highValue;
+			}
+
+			returnCursor = DequeNode.Factory.createNodeAfter(returnCursor, 0);
 			thisCursor = thisCursor.getNext();
-			//TODO: this means the nodes are all signed...
+
+			if (valueRemaining != 0 && thisCursor != null)
+			{
+				lowValue = ((int) (valueRemaining & LOW_MASK_64));
+				highValue = (int) (valueRemaining >>> 32);
+				sum = Integer.toUnsignedLong(thisCursor.getData()) + lowValue;
+
+				returnCursor.setData((int) (sum & LOW_MASK_64));
+				sum >>>=32;
+
+				valueRemaining = sum + highValue;
+				returnCursor = DequeNode.Factory.createNodeAfter(returnCursor, 0);
+				thisCursor = thisCursor.getNext();
+			}
+
 		}
-		returnCursor.remove();  //remove the last one since it has only leading 0s
+		if (valueRemaining != 0)
+		{
+			lowValue = ((int) (valueRemaining & LOW_MASK_64));
+			highValue = (int) (valueRemaining >>> 32);
+			returnCursor.setData(lowValue);
+			returnCursor = DequeNode.Factory.createNodeAfter(returnCursor, highValue);
+		}
+		if(returnCursor.getData().intValue() == 0) returnCursor.remove();  //remove the last one since has only leading 0s
+		//do not use else. this can occur either way
 		return returnValue;
 	}
 
 	public InfiniteInteger add(BigInteger value) {
 		//TODO: currently assumes all positive
+		if(!this.isFinite() || value.compareTo(BigInteger.ZERO) == 0) return this;
 		if(this == POSITIVE_INFINITITY || this == NOT_A_NUMBER) return this;
 		if(value.compareTo(bigIntegerMaxLong) != 1) return add(value.longValue());
 		InfiniteInteger returnValue = this;
@@ -104,11 +179,33 @@ public class InfiniteInteger extends Number {
 
 	public InfiniteInteger add(InfiniteInteger value) {
 		//TODO: currently assumes all positive
-		if(this == POSITIVE_INFINITITY || this == NOT_A_NUMBER || value == ZERO) return this;
-		if(value == POSITIVE_INFINITITY || value == NOT_A_NUMBER || this == ZERO) return value;
+		if(!this.isFinite() || value == ZERO) return this;
+		if(!value.isFinite() || this == ZERO) return value;
 		// TODO method stub
 		return null;
 	}
+
+	public InfiniteInteger subtract(long value) {
+		// TODO method stub
+		return null;
+	}
+
+	public InfiniteInteger subtract(BigInteger value) {
+		// TODO method stub
+		return null;
+	}
+
+	public InfiniteInteger subtract(InfiniteInteger value) {
+		// TODO method stub
+		return null;
+	}
+
+	public boolean isNaN(){return this == NOT_A_NUMBER;}
+	public static boolean isNaN(InfiniteInteger num){return num.isNaN();}
+	public boolean isInfinite(){return (this == POSITIVE_INFINITITY || this == NEGATIVE_INFINITITY);}
+	public static boolean isInfinite(InfiniteInteger num){return num.isInfinite();}
+	public boolean isFinite(){return (!this.isNaN() && !this.isInfinite());}
+	public static boolean isFinite(InfiniteInteger num){return (!num.isNaN() && !num.isInfinite());}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -118,8 +215,8 @@ public class InfiniteInteger extends Number {
 		if(!(obj instanceof InfiniteInteger)) return false;  //includes null check and children
 		InfiniteInteger other = (InfiniteInteger) obj;
 		if(other == ZERO || other == POSITIVE_INFINITITY || other == NEGATIVE_INFINITITY || other == NOT_A_NUMBER) return false;
-		DequeNode<Long> thisCursor = this.magnitudeHead;
-		DequeNode<Long> otherCursor = other.magnitudeHead;
+		DequeNode<Integer> thisCursor = this.magnitudeHead;
+		DequeNode<Integer> otherCursor = other.magnitudeHead;
 		while (thisCursor != null && otherCursor != null)
 		{
 			if(!Objects.equals(thisCursor.getData(), otherCursor.getData())) return false;
@@ -132,15 +229,15 @@ public class InfiniteInteger extends Number {
 	@Override
 	public int hashCode() {
 		//collisions are, in theory, likely
-		DequeNode<Long> cursor = magnitudeHead;
-		long hash = cursor.getData();
+		DequeNode<Integer> cursor = magnitudeHead;
+		int hash = cursor.getData();
 		cursor = cursor.getNext();
 		while (cursor != null)
 		{
 			hash ^= cursor.getData();
 			cursor = cursor.getNext();
 		}
-		return (int) hash;
+		return hash;
 	}
 
 	@Override
@@ -156,6 +253,12 @@ public class InfiniteInteger extends Number {
 
 	public void toFile(File writeToHere) {
 		// TODO method stub it can always fit
+	}
+
+	@Override
+	public InfiniteInteger copy() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
