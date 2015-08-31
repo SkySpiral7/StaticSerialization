70815f54c676e1b0194e7a2e434aca65e397b33a
Ryan Worcester <rworcest@g.emporia.edu>
2015-08-30 20:06:09 -0500

    Created a simple and complete BloomFilter

---
+++ tests/com/github/SkySpiral7/Java/dataStructures/UT_BloomFilter.java
@@ -0,0 +1,41 @@
+package com.github.SkySpiral7.Java.dataStructures;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.BitSet;
+
+import org.junit.Test;
+
+import com.github.SkySpiral7.Java.dataStructures.BloomFilter.BloomHash;
+
+public final class UT_BloomFilter
+{
+
+	@Test
+   public void bloomFilter() {
+		final BloomFilter bloomFilter = new BloomFilter(new BloomHash()
+		{
+			@Override
+			public BitSet hash(Object element){return BitSet.valueOf(new long[]{element.hashCode()});}
+			//although bad practice, the hashCode is returned for the sake of easy testing
+		}
+, 32);
+
+		assertEquals(BitSet.valueOf(new long[]{0}), bloomFilter.getMask());
+		assertFalse(bloomFilter.contains(1));
+
+		bloomFilter.add(1);
+		bloomFilter.add(2);
+		bloomFilter.add(4);
+
+		assertEquals(BitSet.valueOf(new long[]{7}), bloomFilter.getMask());
+		assertTrue(bloomFilter.contains(2));
+		assertTrue(bloomFilter.contains(3));  //false positive is expected behavior
+		assertFalse(bloomFilter.contains(64));  //mask contains no matching bits
+		assertFalse(bloomFilter.contains(21));  //mask contains some matching bits
+		//this unit test is complete: absolutely all requirements have been tested
+   }
+
+}
