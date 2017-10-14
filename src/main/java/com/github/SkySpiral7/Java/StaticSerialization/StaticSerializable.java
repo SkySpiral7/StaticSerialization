7d5fe813e4fcb15cc504d956b788e0fc89827272
Ryan Worcester <rworcest@g.emporia.edu>
2017-10-14 14:56:52 -0500

    StaticSerializable.readFromStream makes most ids easier

--- src/main/java/com/github/SkySpiral7/Java/StaticSerialization/StaticSerializable.java
+++ src/main/java/com/github/SkySpiral7/Java/StaticSerialization/StaticSerializable.java
@@ -1,20 +1,42 @@
 package com.github.SkySpiral7.Java.StaticSerialization;
 
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+/**
+ * <p>A header that contains the class will be included with each element for security.
+ * Most classes don't need a version number so one is not included by default.
+ * Most classes don't need an id so one is not included by default.</p>
+ *
+ * <p>Note that using this interface also requires the class to define a static method of this signature:</p>
+ * <blockQuote>{@code public static YourClass readFromStream(final ObjectStreamReader reader)}</blockQuote>
+ */
 public interface StaticSerializable
 {
    //public static T<this> readFromStream(final ObjectStreamReader reader)
    public void writeToStream(final ObjectStreamWriter writer);
 
-   //   public static <T> T readFromStream(final ObjectStreamReader reader, final Function<ObjectStreamReader, T> emptyCreate,
-   //                                      final BiConsumer<ObjectStreamReader, T> populate)
-   //   {
-   //      final ObjectReaderRegistry registry = reader.getObjectRegistry();
-   //      final T registeredObject = registry.readObjectOrId(reader);
-   //      if (registeredObject != null) return registeredObject;
-   //
-   //      final T result = emptyCreate.apply(reader);
-   //      registry.claimId(result);
-   //      populate.accept(reader, result);
-   //      return result;
-   //   }
+   /**
+    * This static method includes the boilerplate code that is required if your class needs an id in order to avoid
+    * a circular reference. Note that this method assumes that any object the stream asks to create will be created and returned,
+    * if you want to return the same instance rather than a new one (despite the stream) then you must implement this yourself.
+    *
+    * @param reader      the current stream to read from
+    * @param createEmpty a function (likely a method reference) that creates the object with minimal information
+    * @param populate    a consumer (likely a method reference) that populates all other data within the object
+    *
+    * @return the already created object that matches the id or the object created by createEmpty
+    */
+   public static <T> T readFromStream(final ObjectStreamReader reader, final Function<ObjectStreamReader, T> createEmpty,
+                                      final BiConsumer<ObjectStreamReader, T> populate)
+   {
+      final ObjectReaderRegistry registry = reader.getObjectRegistry();
+      final T registeredObject = registry.readObjectOrId(reader);
+      if (registeredObject != null) return registeredObject;
+
+      final T result = createEmpty.apply(reader);
+      registry.claimId(result);
+      populate.accept(reader, result);
+      return result;
+   }
 }
--- src/test/java/com/github/SkySpiral7/Java/StaticSerialization/testClasses/RootedGraph.java
+++ src/test/java/com/github/SkySpiral7/Java/StaticSerialization/testClasses/RootedGraph.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
-import com.github.SkySpiral7.Java.StaticSerialization.ObjectReaderRegistry;
 import com.github.SkySpiral7.Java.StaticSerialization.ObjectStreamReader;
 import com.github.SkySpiral7.Java.StaticSerialization.ObjectStreamWriter;
 import com.github.SkySpiral7.Java.StaticSerialization.StaticSerializable;
@@ -103,40 +102,22 @@ public final class RootedGraph implements StaticSerializable
 
       public static Node readFromStream(final ObjectStreamReader reader)
       {
-         final ObjectReaderRegistry registry = reader.getObjectRegistry();
-         final Node registeredObject = registry.readObjectOrId(reader);
-         if (registeredObject != null) return registeredObject;
+         return StaticSerializable.readFromStream(reader, Node::createEmpty, RootedGraph.Node::populate);
+      }
 
-         final Node result = new Node(reader.readObject(String.class));
-         registry.claimId(result);
+      private static Node createEmpty(final ObjectStreamReader reader)
+      {
+         return new Node(reader.readObject(String.class));
+      }
 
+      private static void populate(final ObjectStreamReader reader, final Node result)
+      {
          final int linkSize = reader.readObject(int.class);
          for (int linkIndex = 0; linkIndex < linkSize; ++linkIndex)
          {
             result.links.add(reader.readObject(Node.class));
          }
-
-         return result;
       }
-
-      //      public static Node readFromStream2(final ObjectStreamReader reader)
-      //      {
-      //         return StaticSerializable.readFromStream(reader, Node::emptyCreate, RootedGraph.Node::populate);
-      //      }
-      //
-      //      public static Node emptyCreate(final ObjectStreamReader reader)
-      //      {
-      //         return new Node(reader.readObject(String.class));
-      //      }
-      //
-      //      public static void populate(final ObjectStreamReader reader, final Node result)
-      //      {
-      //         final int linkSize = reader.readObject(int.class);
-      //         for (int linkIndex = 0; linkIndex < linkSize; ++linkIndex)
-      //         {
-      //            result.links.add(reader.readObject(Node.class));
-      //         }
-      //      }
 
       @Override
       public void writeToStream(final ObjectStreamWriter writer)
